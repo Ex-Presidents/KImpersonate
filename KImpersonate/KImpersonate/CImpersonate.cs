a6c6bb24c36a68dcb877e95f6714946af5349a93
Kr4ken-9 <Kr4kenKT@gmail.com>
2016-11-17 19:04:18 -0600

    Initial Push

---
+++ KImpersonate/KImpersonate/CImpersonate.cs
@@ -0,0 +1,58 @@
+﻿using Rocket.API;
+using Steamworks;
+using SDG.Unturned;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KImpersonate
+{
+    public class CImpersonate : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller { get { return AllowedCaller.Both; } }
+        public string Name { get { return "Impersonate"; } }
+        public string Help { get { return "Impersonate a player through text"; } }
+        public string Syntax { get { return "/impersonate <player> [message]"; } }
+        public List<string> Aliases { get { return new List<string>(); } }
+        public List<string> Permissions { get { return new List<string> { "KImpersonate.impersonate" }; } }
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            if (command.Length != 2)
+            {
+                if (caller.Id == "Console")
+                {
+                    Rocket.Core.Logging.Logger.Log("The correct syntax is, /Impersonate <player> '[message]'");
+                    return;
+                }
+                else
+                {
+                    UnturnedChat.Say(caller, "The correct syntax is, '/Impersonate <player> [message]'");
+                    return;
+                }
+            }
+            if (UnturnedPlayer.FromName(command[0]) == null)
+            {
+                if (caller.Id == "Console")
+                {
+                    Rocket.Core.Logging.Logger.Log("Could not find player, '" + command[0] + "'.");
+                    return;
+                }
+                else
+                {
+                    UnturnedChat.Say(caller, "Could not find player, '" + command[0] + "'.");
+                    return;
+                }
+            }
+
+            UnturnedPlayer target = UnturnedPlayer.FromName(command[0]);
+
+            foreach (string m in UnturnedChat.wrapMessage(command[1]))
+                ChatManager.instance.channel.send("tellChat", ESteamCall.OTHERS, ESteamPacket.UPDATE_RELIABLE_BUFFER, new object[] { target.CSteamID, (byte)EChatMode.GLOBAL, target.Color, m });
+        }
+    }
+}
---
+++ KImpersonate/KImpersonate/KImpersonate.cs
@@ -0,0 +1,22 @@
+﻿using Rocket.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.API;
+
+namespace KImpersonate
+{
+    public class KImpersonate : RocketPlugin
+    {
+        protected override void Load()
+        {
+            Rocket.Core.Logging.Logger.Log("Kr4ken Impersonate loaded!");
+        }
+        protected override void Unload()
+        {
+            Rocket.Core.Logging.Logger.Log("Kr4ken Impersonate unloaded!");
+        }
+    }
+}
